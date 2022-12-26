1db42e308e948fc21d3b47f193e989a3acae6fa4
Your Name <you@example.com>
2022-12-26 16:06:45 +0300

    add filtering switches

--- lib/screen_widgets/ayarlar_ekrani.dart
+++ lib/screen_widgets/ayarlar_ekrani.dart
@@ -1,8 +1,39 @@
 import 'package:flutter/material.dart';
 import '../normal_widgets/main_drawer.dart';
 
-class AyarlarEkrani extends StatelessWidget {
+class AyarlarEkrani extends StatefulWidget {
+  // which filters set or not we dont know
+// change the stateless widget into statefull widget (1)
   static const routeName = '/ayarlar';
+
+  @override
+  State<AyarlarEkrani> createState() => _AyarlarEkraniState();
+}
+
+class _AyarlarEkraniState extends State<AyarlarEkrani> {
+  // add new propetries in the AyarlarEkrani widget (2)
+  var _glutenFree = false;
+  var _vegeterian = false;
+  var _vegan = false;
+  var _lactoseFree = false;
+
+  Widget _buildSwitchListTile(
+    // create a build method (6)
+    // add a widget properties
+    String title,
+    String description,
+    bool currentValue,
+    Function updateValue,
+  ) {
+    return SwitchListTile(
+      // pass the properties based on above (7)
+      title: Text(title),
+      value: currentValue,
+      subtitle: Text(description),
+      onChanged: updateValue,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -10,13 +41,64 @@ class AyarlarEkrani extends StatelessWidget {
         title: Text('Ayarlarim'),
       ),
       drawer: MainDrawer(),
-      body: Center(
-        child: Text(
-          'Ayarlar!',
-          style: TextStyle(
-            color: Colors.black,
+      body: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.all(20),
+            child: Text(
+              'Yemek secimini ayarla',
+              style: Theme.of(context).textTheme.titleMedium,
+            ),
           ),
-        ),
+          Expanded(
+              child: ListView(
+            children: [
+              _buildSwitchListTile(
+                'Gluuten-free',
+                'Sadece gluteen-free yemekleri icerir',
+                _glutenFree,
+                (newValue) {
+                  // this is an updated value (8)
+                  setState(() {
+                    _glutenFree = newValue; // defined just above
+                  });
+                },
+              ),
+
+              // Do it for other var properties which are defined above (9)
+              _buildSwitchListTile(
+                'Lactose-free',
+                'Sadece lactose-free yemekleri icerir',
+                _lactoseFree,
+                (newValue) {
+                  setState(() {
+                    _lactoseFree = newValue;
+                  });
+                },
+              ),
+              _buildSwitchListTile(
+                'Vegeterian',
+                'Sadece vejeretyan yemekleri icerir',
+                _vegeterian,
+                (newValue) {
+                  setState(() {
+                    _vegeterian = newValue;
+                  });
+                },
+              ),
+              _buildSwitchListTile(
+                'Vegan',
+                'Sadece vegan yemekleri icerir',
+                _vegan,
+                (newValue) {
+                  setState(() {
+                    _vegan = newValue;
+                  });
+                },
+              ),
+            ],
+          ))
+        ],
       ),
     );
   }
